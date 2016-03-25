e60c521c0b0283100fec7ecdd585f24cf19f1e3e
P3nny <ennenbach@worthandlung.de>
2016-03-25 01:57:35 +0100

    Startet forms, not working because of requirements/miniconda

--- 8/app/views.py
+++ 8/app/views.py
@@ -1,5 +1,6 @@
 from random import randint
 from random import choice
+from forms import GetLucky
 
 from app import app
 from flask import render_template
@@ -16,14 +17,29 @@ def lucky_static():
 
 @app.route('/<name>/')
 def name_length(name):
-	length = len(name)
-	return render_template('name.html', name=name.capitalize(), length=length)
+    length = len(name)
+    return render_template('name.html', name=name.capitalize(), length=length)
 
 @app.route('/horoskop/')
 def get_horoskop():
-	a = ['In deinen Sternen steht, dass ', 'Auf dem Grunde der Kaffeetasse ist zu lesen, dass ', 'Das Orakel sagt, dass ']
-	b = ['Du bald ', 'Du für immer und ewig ', 'Du in deinem Leben ']
-	c = ['glücklich wirst.', 'Liebe findest.', 'reich und berühmt wirst.']
-	horoskop = choice(a)+choice(b)+choice(c)
-	lucky_num = randint(1, 100)
-	return render_template('horoskop.html', horoskop=horoskop, lucky_num=lucky_num)
+    a = ['In deinen Sternen steht, dass ', 'Auf dem Grunde der Kaffeetasse ist zu lesen, dass ', 'Das Orakel sagt, dass ']
+    b = ['Du bald ', 'Du für immer und ewig ', 'Du in deinem Leben ']
+    c = ['glücklich wirst.', 'Liebe findest.', 'reich und berühmt wirst.']
+    horoskop = choice(a)+choice(b)+choice(c)
+    lucky_num = randint(1, 100)
+    return render_template('horoskop.html', horoskop=horoskop, lucky_num=lucky_num)
+
+@app.route('/game/')
+def game():
+    game_nums = []
+    while len(game_nums) < 5:
+        n = randint(1, 10)
+        if not n in game_nums:
+            game_nums.append(n)
+    return render_template('game.html', game_nums = game_nums)
+
+@app.route('/nums/', methods=['GET', 'POST'])
+def get_nums():
+	form = GetLucky()
+	return render_template('get_lucky.html', form=form)
+
